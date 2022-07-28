31a899d7ce3d71db581630b29118dff19f0de30f
stancatimartin <stancatimartin@gmail.com>
2022-07-28 11:09:27 -0300

    fecha de nacimiento2

--- mvt/mvt/urls.py
+++ mvt/mvt/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from mvt.views import saludo, segundo_template, template_con_lista
+#from mvt.views import saludo, segundo_template, template_con_lista
 from familiares.views import create_familiar, lista_familiar
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('saludo/', saludo, name='saludo'),
-    path('segundo_template/', segundo_template, name= 'segundo_template'),
-    path('template_con_lista/', template_con_lista, name= 'template_con_lista'),
+#    path('saludo/', saludo, name='saludo'),
+#    path('segundo_template/', segundo_template, name= 'segundo_template'),
+#    path('template_con_lista/', template_con_lista, name= 'template_con_lista'),
     path('create_familiar/', create_familiar, name= 'create_familiar'),
     path('lista_familiar/', lista_familiar, name= 'lista_familiar')
     ]
